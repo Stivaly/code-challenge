5f6a5b482001768ca3e68f106d1ef24e83b4c697
Stivaly <stivaly.g@hotmail.com>
2023-10-12 15:55:08 -0300

    Agrego limpieza adicional por inconvenientes en la carga del csv a bigquery, así como llamo el proceso de carga

--- src/etl.py
+++ src/etl.py
@@ -1,29 +1,64 @@
+from bigquery_utils import load_data
 import pandas as pd
 import os
+
 # Realizamos referencia a nuestra llave
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys/etl-code-challenge-42c88c66e88d.json"
 
+def extract():
+    try:
+        # Leer el archivo CSV con el Framework PANDAS
+        online_retail = pd.read_csv("../data/source/online_retail.csv", encoding='utf-8')
+        return online_retail
+    except Exception as e:
+        print(f"Error durante la extracción: {e}")
+        raise
 
-def extract_and_process(file):
-    # Leer el archivo CSV con el Framework PANDAS
-    online_retail = pd.read_csv(file, encoding='utf-8')
+def transform(online_retail):
+    # Operaciones de limpieza y transformación para archivo de Retail de ventas
+    try:
+        """
+        Durante la exploracion en python shell pude detectar inconsistencias en algunos campos con una letra adicional,
+        al verificar no afecta quitar esta letra y nos permite cambiar el campo InvoiceNo a numero, manteniendo sus valores sin duplicidad
+        """
+        online_retail['InvoiceNo'] = online_retail['InvoiceNo'].str.replace('[a-zA-Z]', '', regex=True)
+        # Damos los formatos correctos a las columnas para poder ser procesadas correctamente en BigQuery
+        online_retail['InvoiceNo'] = online_retail['InvoiceNo'].astype(int)
+        online_retail['InvoiceDate'] = pd.to_datetime(online_retail['InvoiceDate'])
 
-    # Operaciones básicas de limpieza y transformación para archivo de Retail de ventas entre 01/12/2010 y 09/12/2011
-    # Eliminar registros duplicados
-    online_retail.drop_duplicates(inplace=True)
 
-    # Reemplazo de valores faltantes en columnas numéricas con 0
-    numerical_columns = online_retail.select_dtypes(include=['number']).columns
-    online_retail[numerical_columns] = online_retail[numerical_columns].fillna(0)
+        # Eliminar registros duplicados, solo cuando todos los campos son exactamente iguales
+        online_retail.drop_duplicates(inplace=True)
 
-    # Reemplazo de valores faltantes en columnas tipo object con 'sin información'
-    object_columns = online_retail.select_dtypes(include=['object']).columns
-    online_retail[object_columns] = online_retail[object_columns].fillna('sin informacion')
+        # Reemplazo de valores faltantes en columnas numéricas con 0
+        numerical_columns = ['InvoiceNo', 'Quantity', 'UnitPrice', 'CustomerID']
+        online_retail[numerical_columns] = online_retail[numerical_columns].fillna(0)
 
-    return online_retail
+        # Reemplazo de valores faltantes en columnas string con 'sin información'
+        some_columns = ['StockCode', 'Description', 'Country']
+        online_retail[some_columns] = online_retail[some_columns].fillna('sin informacion')
 
-# Ejecutar la función
-processed_online_retail = extract_and_process("../data/source/online_retail.csv")
+        #Detectamos que la columna description tiene comillas simples que estan generando errores de cargado, usamos el siguiente codigo para reemplazar las comillas simples y dobles
+        online_retail['Description'] = online_retail['Description'].str.replace('["\']', '', regex=True)
 
-prueba = processed_online_retail.query("CustomerID == 'sin informacion'")
-print(prueba.head())
+        return online_retail
+    except Exception as e:
+        print(f"Error durante la transformación: {e}")
+        raise
+
+def prepare():
+    try:
+        #Transformamos archivo a CSV para su facilidad de subida
+        online_retail = extract()
+        processed_online_retail = transform(online_retail)
+        processed_online_retail.to_csv("../data/temporales/temp_data.csv", index=False, encoding='utf-8')
+        return processed_online_retail
+    except Exception as e:
+        print(f"Error durante la preparación: {e}")
+        raise
+
+try:
+    prepared_data = prepare()
+    load_data('online_retail', 'sales_transaction', '../data/temporales/temp_data.csv')
+except Exception as e:
+    print(f"Error general en el ETL: {e}")
